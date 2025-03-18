db1ae4ac03113911ae2003d9a6669cf39ccd6a99
danishanr <danishaanr@gmail.com>
2025-03-18 19:02:01 +0700

    Pembuatan Program Perulangan

---
+++ perulanganfor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Pencacah Naik
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Nilai i = " << i << " Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;    
+
+
+
+    //Pencacah Turun
+    for (i = 4; i > 0; i--)
+    {
+        cout << "Nilai i = " << i << " Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;    
+}
